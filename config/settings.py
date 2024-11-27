61eaf57c3807db10d1bddc14fa7fb9fb6b61930a
seungwon0113 <dhkd0113@naver.com>
2024-11-28 00:05:00 +0900

    app added

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # apps
+    "user",
+    "calendars",
+    "event",
+    "comment",
+    "comment_like",
+    "admin",
+    "subscription",
+    "notification",
+    "location",
 ]
 
 MIDDLEWARE = [
