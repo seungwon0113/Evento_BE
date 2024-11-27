11e9ccbf7d600049a64e898bf267fac02fd2f9ab
seungwon0113 <dhkd0113@naver.com>
2024-11-28 02:44:19 +0900

    - ci python test

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 
